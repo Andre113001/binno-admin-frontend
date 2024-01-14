3e1eb569f2d23bf60807313db77215dde3db6c70
Ted30 <teddymarcenaje@gmail.com>
2024-01-15 07:23:43 +0800

    Application processing

--- src/components/RadioButton/RadioButton.jsx
+++ src/components/RadioButton/RadioButton.jsx
@@ -1,41 +1,40 @@
-import * as React from "react";
-import Radio from "@mui/material/Radio";
+import * as React from 'react'
+import Radio from '@mui/material/Radio'
 
-import styles from "./RadioButton.module.css";
+import styles from './RadioButton.module.css'
 
 export default function RadioButton(props) {
-    const { data } = props;
+    const { data, onSelect, selected } = props
 
-    const [selectedValue, setSelectedValue] = React.useState("Zoom");
+    const [selectedValue, setSelectedValue] = React.useState(selected)
 
     const handleChange = (event) => {
-        setSelectedValue(event.target.value);
-    };
-
-    console.log(data);
+        setSelectedValue(event.target.value)
+        onSelect(event.target.value)
+    }
 
     const content = data.map((value) => (
-        <div className={`${styles["radio-button"]}`}>
+        <div className={`${styles['radio-button']}`}>
             <Radio
                 checked={selectedValue === value}
                 onChange={handleChange}
                 value={value}
                 name="radio-buttons"
-                inputProps={{ "aria-label": "A" }}
+                inputProps={{ 'aria-label': 'A' }}
                 sx={{
-                    color: "#BEBEBE",
-                    "&.Mui-checked": {
-                        color: "#616161",
+                    color: '#BEBEBE',
+                    '&.Mui-checked': {
+                        color: '#616161',
                     },
                 }}
             />
             <p>{value}</p>
         </div>
-    ));
+    ))
 
     return (
-        <div className={`${styles["container"]}`} {...props}>
+        <div className={`${styles['container']}`} {...props}>
             {content}
         </div>
-    );
+    )
 }
