2ca3a9548524ba87e094dd4360133c05acf33ba3
Trentcrypt3d <70151180+Trentcrypt3d@users.noreply.github.com>
2024-02-19 16:47:03 +0800

    added status column in membership management

--- src/pages/MembershipManagement/MembershipManagement.jsx
+++ src/pages/MembershipManagement/MembershipManagement.jsx
@@ -2,9 +2,8 @@ import { Fragment, useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import InputAdornment from "@mui/material/InputAdornment";
 
-
-import styles from './MembershipManagement.module.css';
-import processingStyle from '../ApplicationProcessing/ApplicationProcessing.module.css';
+import styles from "./MembershipManagement.module.css";
+import processingStyle from "../ApplicationProcessing/ApplicationProcessing.module.css";
 import SearchIcon from "@mui/icons-material/Search";
 
 // import Topbar from "../../components/Topbar/Topbar";
@@ -16,115 +15,118 @@ import Topbar from "../../components/Topbar/Topbar";
 import useHttp from "../../hooks/http-hook";
 
 const MembershipManagement = () => {
-    const [ members, setMembers ] = useState();
-    const { sendRequest, isLoading} = useHttp();
-    const [selectedOption, setSelectedOption] = useState(1); // Initial selected option
-    const [ appCount, setAppCount ] = useState();
-    const navigate = useNavigate();
+  const [members, setMembers] = useState();
+  const { sendRequest, isLoading } = useHttp();
+  const [selectedOption, setSelectedOption] = useState(1); // Initial selected option
+  const [appCount, setAppCount] = useState();
+  const navigate = useNavigate();
 
-    const handleSelect = (value) => {
-        setSelectedOption(value);
+  const handleSelect = (value) => {
+    setSelectedOption(value);
+  };
+
+  useEffect(() => {
+    const loadData = async () => {
+      const result = await sendRequest({
+        url: `${import.meta.env.VITE_BACKEND_DOMAIN}/get/users`,
+      });
+
+      const fetchApp = await sendRequest({
+        url: `${import.meta.env.VITE_BACKEND_DOMAIN}/application`,
+      });
+
+      setAppCount(fetchApp.length);
+      setMembers(result);
     };
 
-    useEffect(() => {
-        const loadData = async () => {
-            const result = await sendRequest({
-                url: `${import.meta.env.VITE_BACKEND_DOMAIN}/get/users`,
-            });
+    loadData();
+  }, [sendRequest]);
 
-            const fetchApp = await sendRequest({
-                url: `${import.meta.env.VITE_BACKEND_DOMAIN}/application`
-            })
-            
-            setAppCount(fetchApp.length);
-            setMembers(result);
-        }
+  return (
+    <Fragment>
+      <Topbar />
+      <div className="container">
+        <div onClick={() => navigate(-1)}>
+          <Back />
+        </div>
 
-        loadData();
-    }, [sendRequest])
+        <div className={`${styles["management-title"]}`}>
+          <div className={`${styles["title"]}`}>
+            <h1>Members</h1>
+          </div>
 
-    return (
-        <Fragment>
-            <Topbar />
-            <div className="container">
-                <div onClick={() => navigate(-1)}>
-                    <Back />
-                </div>
+          <Link
+            to={"/applications"}
+            className={`${styles["management-request"]}`}
+          >
+            <p>Request for Application</p>
+            <div className={`${styles["circle"]}`}>{appCount}</div>
+          </Link>
+        </div>
 
-                <div className={`${styles['management-title']}`}>
-                    <div className={`${styles['title']}`}>
-                        <h1>Members</h1>
-                    </div>
+        <div className={`${processingStyle["filters"]}`}>
+          <div className={`${processingStyle["section_1"]}`}>
+            <Dropdown
+              selected={selectedOption}
+              onSelect={handleSelect}
+              options={[
+                { value: 1, label: "Enabler" },
+                { value: 2, label: "Company" },
+              ]}
+            />
+          </div>
 
-                    <Link to={'/applications'} className={`${styles['management-request']}`}>
-                        <p>Request for Application</p>
-                        <div className={`${styles['circle']}`} >
-                            {appCount}
-                        </div>
-                    </Link>
-                </div>
-
-                <div className={`${processingStyle["filters"]}`}>
-                    <div className={`${processingStyle["section_1"]}`}>
-                    <Dropdown
-                        selected={selectedOption}
-                        onSelect={handleSelect}
-                        options={[
-                            { value: 1, label: 'Enabler' },
-                            { value: 2, label: 'Company' },
-                        ]}
-                    />
-                    </div>
-
-                    <div>
-                        <div>
-                            <TextField
-                                placeholder="Search"
-                                InputProps={{
-                                    startAdornment: (
-                                        <InputAdornment position="start">
-                                            <SearchIcon />
-                                        </InputAdornment>
-                                    ),
-                                }}
-                                sx={{
-                                    height: "35px",
-                                    width: '398px',
-                                    "& input": {
-                                        padding: "6px 0 6px 0", // Adjust the padding if needed
-                                    },
-                                    "& .MuiOutlinedInput-root": {
-                                        borderRadius: "10px",
-                                    },
-                                }}
-                            />
-                        </div>
-                    </div>
-                </div>
-
-                <div className={`${processingStyle["application-processing-table"]}`}>
-                    <table width={"100%"}>
-                        <thead>
-                            <tr className={`${processingStyle["table-heading"]}`}>
-                                <th>Member's ID</th>
-                                <th>Name of Institution</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody className={`${processingStyle["table-body"]}`}>
-                            {members?.map((member) => (
-                                <tr className={`${processingStyle["table-row"]}`} key={member.member_id}>
-                                <MembershipManagementList
-                                    data={member}
-                                />
-                            </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
+          <div>
+            <div>
+              <TextField
+                placeholder="Search"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <SearchIcon />
+                    </InputAdornment>
+                  ),
+                }}
+                sx={{
+                  height: "35px",
+                  width: "398px",
+                  "& input": {
+                    padding: "6px 0 6px 0", // Adjust the padding if needed
+                  },
+                  "& .MuiOutlinedInput-root": {
+                    borderRadius: "10px",
+                  },
+                }}
+              />
             </div>
-        </Fragment>
-    );
+          </div>
+        </div>
+
+        <div className={`${processingStyle["application-processing-table"]}`}>
+          <table width={"100%"}>
+            <thead>
+              <tr className={`${processingStyle["table-heading"]}`}>
+                <th>Member's ID</th>
+                <th>Status</th>
+                <th>Name of Institution</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody className={`${processingStyle["table-body"]}`}>
+              {members?.map((member) => (
+                <tr
+                  className={`${processingStyle["table-row"]}`}
+                  key={member.member_id}
+                >
+                  <MembershipManagementList data={member} />
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </Fragment>
+  );
 };
 
 export default MembershipManagement;
--- src/pages/MembershipManagement/MembershipManagementList.jsx
+++ src/pages/MembershipManagement/MembershipManagementList.jsx
@@ -5,7 +5,7 @@ import photo from "../../assets/Screenshot (549).png";
 
 import styles from "../ApplicationProcessing/ApplicationProcessing.module.css";
 import managementStyles from "./MembershipManagement.module.css";
-import {Radio, RadioGroup, FormControlLabel} from '@mui/material'
+import { Radio, RadioGroup, FormControlLabel } from "@mui/material";
 
 import RespondIcon from "../ApplicationProcessing/RespondIcon";
 import ActivitiesIcon from "./ActivitiesIcon";
@@ -15,161 +15,177 @@ import { Button } from "@mui/material";
 import { useTimer } from "react-timer-hook";
 
 const MembershipManagementList = (props) => {
-    const [openModal, setOpenModal] = useState(false);
-    const [duration, setDuration] = useState('7days'); // Default duration is 7 days`
-    const [seconds, setSeconds] = useState(30); // Initial timer value for permanent suspension
-    const [startTimer, setStartTimer] = useState(false);
-    const [selectedUser, setSelectedUser] = useState();
-    const { onOpenModal } = props;
-    const memberData = props.data;
+  const [openModal, setOpenModal] = useState(false);
+  const [duration, setDuration] = useState("7days"); // Default duration is 7 days`
+  const [seconds, setSeconds] = useState(30); // Initial timer value for permanent suspension
+  const [startTimer, setStartTimer] = useState(false);
+  const [selectedUser, setSelectedUser] = useState();
+  const { onOpenModal } = props;
+  const memberData = props.data;
 
-    const modalHandler = (user) => {
-        setOpenModal(true);
-        setSelectedUser(user)
-        // startTimer();
-        setStartTimer(true);
-    };
+  const modalHandler = (user) => {
+    setOpenModal(true);
+    setSelectedUser(user);
+    // startTimer();
+    setStartTimer(true);
+  };
 
-    const handleClose = () => {
-        setOpenModal(false);
-        setStartTimer(false);
-        setSeconds(30)
-    };
+  const handleClose = () => {
+    setOpenModal(false);
+    setStartTimer(false);
+    setSeconds(30);
+  };
 
-    const handleRadioChange = (event) => {
-        setDuration(event.target.value);
-    };
+  const handleRadioChange = (event) => {
+    setDuration(event.target.value);
+  };
 
-    useEffect(() => {
-        let intervalId;
-    
-        if (startTimer) {
-          intervalId = setInterval(() => {
-            setSeconds(prevSeconds => {
-              if (prevSeconds === 1) {
-                clearInterval(intervalId);
-                setStartTimer(false);
-                return 0;
-              }
-              return prevSeconds - 1;
-            });
-          }, 1000);
-        }
-    
-        return () => clearInterval(intervalId);
-      }, [startTimer]);
-      
+  useEffect(() => {
+    let intervalId;
 
-    const handleConfirmRestrict = () => {
-        console.log({
-            user: selectedUser,
-            duration: duration
+    if (startTimer) {
+      intervalId = setInterval(() => {
+        setSeconds((prevSeconds) => {
+          if (prevSeconds === 1) {
+            clearInterval(intervalId);
+            setStartTimer(false);
+            return 0;
+          }
+          return prevSeconds - 1;
         });
-    };
+      }, 1000);
+    }
 
-    // console.log(memberData);
+    return () => clearInterval(intervalId);
+  }, [startTimer]);
 
-    return (
-        <Fragment>
-            <td>
-                <p>{memberData.member_id}</p>
-            </td>
-            <td className={`${managementStyles["institution-col"]}`}>
-                <img src={`http://217.196.51.115/m/api/images?filePath=profile-img/${memberData.setting_profilepic}`} alt="" />
-                <div className={`${managementStyles["institution-data"]}`}>
-                    <div style={{fontWeight: 'bold'}}>{memberData.setting_institution}</div>
-                    <div>{memberData.setting_address}</div>
-                </div>
-            </td>
-            <td className={`${managementStyles["action"]}`}>
-                <div className={`${managementStyles["action-button"]}`}>
-                    <div className={`${managementStyles["schedule"]}`} onClick={() => modalHandler(memberData)}>
-                        {/* <ScheduleIcon /> */}
-                        <div >
-                            <TimeOut />
-                        </div>
-                        <button
-                            className={`${managementStyles["timeout-button"]}`}
-                        >
-                            Restrict
-                        </button>
-                    </div>
+  const handleConfirmRestrict = () => {
+    console.log({
+      user: selectedUser,
+      duration: duration,
+    });
+  };
 
-                    <div className={`${managementStyles["border"]}`}></div>
+  // console.log(memberData);
 
-                    <div className={`${managementStyles["schedule"]}`}>
-                        <Link
-                            to={`/membership/${memberData.setting_memberId}`}
-                            className={`${managementStyles["member-button"]}`}
-                        >
-                            View Member
-                        </Link>
-                    </div>
-                </div>
-            </td>
+  return (
+    <Fragment>
+      <td>
+        <p>{memberData.member_id}</p>
+      </td>
+      <td>
+        <div className={`${managementStyles["institution-status"]}`}>
+          <div className={`${managementStyles["restricted-status"]}`}></div>
+          {/* <p>{memberData.status}</p> */}
+          <p>Status</p>
+        </div>
+      </td>
+      <td className={`${managementStyles["institution-col"]}`}>
+        <img
+          src={`http://217.196.51.115/m/api/images?filePath=profile-img/${memberData.setting_profilepic}`}
+          alt=""
+        />
+        <div className={`${managementStyles["institution-data"]}`}>
+          <div style={{ fontWeight: "bold" }}>
+            {memberData.setting_institution}
+          </div>
+          <div>{memberData.setting_address}</div>
+        </div>
+      </td>
+      <td className={`${managementStyles["action"]}`}>
+        <div className={`${managementStyles["action-button"]}`}>
+          <div
+            className={`${managementStyles["schedule"]}`}
+            onClick={() => modalHandler(memberData)}
+          >
+            {/* <ScheduleIcon /> */}
+            <div>
+              <TimeOut />
+            </div>
+            <button className={`${managementStyles["timeout-button"]}`}>
+              Restrict
+            </button>
+          </div>
 
-            <MembershipManagementModal open={openModal} handleClose={handleClose}>
-                <div className={`${managementStyles["modal-main"]}`}>
-                    <div className={`${managementStyles["question"]}`}>
-                        <h1>Are you sure?</h1>
-                        <p>
-                            Do you really want to suspend {selectedUser?.setting_institution}?
-                        </p>
-                    </div>
+          <div className={`${managementStyles["border"]}`}></div>
 
-                    <RadioGroup value={duration} onChange={handleRadioChange}>
-                        <FormControlLabel value="7days" control={<Radio />} label="7 days" />
-                        <FormControlLabel
-                            value="1month"
-                            control={<Radio />}
-                            label="1 month"
-                        />
-                        <FormControlLabel
-                            value="permanent"
-                            control={<Radio />}
-                            label={startTimer ? (`Permanent (${seconds}s)`) : ('Permanent')}
-                            disabled={startTimer}
-                        />
-                    </RadioGroup>
+          <div className={`${managementStyles["schedule"]}`}>
+            <Link
+              to={`/membership/${memberData.setting_memberId}`}
+              className={`${managementStyles["member-button"]}`}
+            >
+              View Member
+            </Link>
+          </div>
+        </div>
+      </td>
 
-                    <div className={`${managementStyles['modal-button']}`}>
-                        <Button 
-                            sx={{
-                                height: '50px', 
-                                background: '#FF7A00', 
-                                border: '1px solid #FF7A00', 
-                                width: '140px', 
-                                borderRadius: '10px', 
-                                '&:hover': {
-                                    background: '#FF7A00'
-                                }, 
-                                color: '#fff', 
-                                fontSize: '18px', 
-                                fontWeight: '500'
-                            }}
-                            onClick={handleConfirmRestrict}
-                        >
-                            Confirm
-                        </Button>
-                        <Button 
-                            sx={{
-                                height: '50px', 
-                                border: '1px solid #000', 
-                                width: '140px', 
-                                borderRadius: '10px', 
-                                fontSize: '18px', 
-                                fontWeight: '500', 
-                                color: '#000'
-                            }}
-                            onClick={handleClose}
-                        >
-                            Discard
-                        </Button>
-                    </div>
-                </div>
-            </MembershipManagementModal>
-        </Fragment>
-    );
+      <MembershipManagementModal open={openModal} handleClose={handleClose}>
+        <div className={`${managementStyles["modal-main"]}`}>
+          <div className={`${managementStyles["question"]}`}>
+            <h1>Are you sure?</h1>
+            <p>
+              Do you really want to suspend {selectedUser?.setting_institution}?
+            </p>
+          </div>
+
+          <RadioGroup value={duration} onChange={handleRadioChange}>
+            <FormControlLabel
+              value="7days"
+              control={<Radio />}
+              label="7 days"
+            />
+            <FormControlLabel
+              value="1month"
+              control={<Radio />}
+              label="1 month"
+            />
+            <FormControlLabel
+              value="permanent"
+              control={<Radio />}
+              label={startTimer ? `Permanent (${seconds}s)` : "Permanent"}
+              disabled={startTimer}
+            />
+          </RadioGroup>
+
+          <div className={`${managementStyles["modal-button"]}`}>
+            <Button
+              sx={{
+                height: "50px",
+                background: "#FF7A00",
+                border: "1px solid #FF7A00",
+                width: "140px",
+                borderRadius: "10px",
+                "&:hover": {
+                  background: "#FF7A00",
+                },
+                color: "#fff",
+                fontSize: "18px",
+                fontWeight: "500",
+              }}
+              onClick={handleConfirmRestrict}
+            >
+              Confirm
+            </Button>
+            <Button
+              sx={{
+                height: "50px",
+                border: "1px solid #000",
+                width: "140px",
+                borderRadius: "10px",
+                fontSize: "18px",
+                fontWeight: "500",
+                color: "#000",
+              }}
+              onClick={handleClose}
+            >
+              Discard
+            </Button>
+          </div>
+        </div>
+      </MembershipManagementModal>
+    </Fragment>
+  );
 };
 
 export default MembershipManagementList;
