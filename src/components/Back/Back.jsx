83377f0856cb7392be5847c02dca13a2ddf5f374
Trentcrypt3d <70151180+Trentcrypt3d@users.noreply.github.com>
2024-03-13 10:41:54 +0800

    adjusted back button design

--- src/components/Back/Back.jsx
+++ src/components/Back/Back.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import {ArrowBack as ArrowBackIcon} from '@mui/icons-material'
+import React from "react";
+import { Link } from "react-router-dom";
+import { ArrowBack as ArrowBackIcon } from "@mui/icons-material";
 
 function Back(props) {
-    return (
-        <div>
-                <div className="flex space-x-4 mb-5 mt-10">
-                    <Link to={props.link}>
-                        <div className="icon cursor-pointer flex row-auto justify-center items-center" >
-                            <ArrowBackIcon />
-                            <span>Back</span>
-                        </div>
-                    </Link>
-                </div>
-        </div>
-  )
+  return (
+    <div>
+      <div className="flex items-center space-x-4 mb-5 mt-10">
+        <Link to={props.link}>
+          <div className="icon cursor-pointer flex row-auto justify-center items-center bg-primary py-2 px-4 rounded-xl text-white">
+            <ArrowBackIcon />
+            <span>Back</span>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default Back
+export default Back;
