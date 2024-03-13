8f11ed3901e35f2171091b5ccd3236b1758d0265
Trentcrypt3d <70151180+Trentcrypt3d@users.noreply.github.com>
2024-03-13 15:14:31 +0800

    adjusted side bar active effect

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -7,9 +7,36 @@ import {
   HelpRounded,
   LogoutRounded,
 } from "@mui/icons-material";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useNavigate, Link, useLocation } from "react-router-dom";
 
 const Sidebar = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [activeLink, setActiveLink] = useState(null);
+
+  useEffect(() => {
+    setActiveLink(location.pathname);
+  }, [location.pathname]);
+
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
+  };
+
+  function handleDestroyToken() {
+    localStorage.removeItem("access");
+    console.log("Token Destroyed");
+    navigate("/");
+  }
+
+  const sidebarLinks = [
+    { to: "/dashboard", icon: <Dashboard />, label: "Dashboard" },
+    { to: "/calendar", icon: <CalendarMonth />, label: "Calendar" },
+    { to: "/activities", icon: <AccessTime />, label: "Activities" },
+    { to: "/membership", icon: <Person />, label: "Members" },
+    { to: "/inquiries", icon: <HelpRounded />, label: "Inquiries" },
+  ];
+
   return (
     <Fragment>
       <div className="fixed top-0 left-0 h-full bg-[#e8e8e8] w-[17%] shadow">
@@ -25,39 +52,25 @@ const Sidebar = () => {
         </div>
         <div className="mt-10 justify-center items-center">
           <div className="flex flex-col justify-start items-start space-y-5 text-xl ml-[20%]">
-            <Link to="/dashboard">
-              <div className="cursor-pointer text-secondary bg-gradient-to-tr from-white to-secondary p-2 pr-[2rem] border border-blue-500 rounded-xl space-x-5 transition duration-100 ease-in-out flex justify-center items-center">
-                <Dashboard />
-                <span>Dashboard</span>
-              </div>
-            </Link>
-            <Link to="/calendar">
-              <div className="cursor-pointer text-gray-700 hover:text-primary space-x-5 transition duration-100 ease-in-out p-3 flex justify-center items-center">
-                <CalendarMonth />
-                <span>Calendar</span>
-              </div>
-            </Link>
-            <Link to="/activities">
-              <div className="cursor-pointer text-gray-700 hover:text-primary space-x-5 transition duration-100 ease-in-out p-3 flex justify-center items-center">
-                <AccessTime />
-                <span>Activities</span>
-              </div>
-            </Link>
-            <Link to="/membership">
-              <div className="cursor-pointer text-gray-700 hover:text-primary space-x-5 transition duration-100 ease-in-out p-3 flex justify-center items-center">
-                <Person />
-                <span>Members</span>
-              </div>
-            </Link>
-            <Link to="/inquiries">
-              <div className="cursor-pointer text-gray-700 hover:text-primary space-x-5 transition duration-100 ease-in-out p-3 flex justify-center items-center">
-                <HelpRounded />
-                <span>Inquiries</span>
-              </div>
-            </Link>
+            {sidebarLinks.map((link, index) => (
+              <Link
+                key={index}
+                to={link.to}
+                className={`cursor-pointer text-gray-700 bg-gradient-to-r ${
+                  activeLink === link.to
+                    ? "from-gray-200 from-20% via-white via-40% to-secondary to-100% text-secondary"
+                    : "bg-gray-200 border-none"
+                } p-2 pr-[2rem] border border-blue-500 rounded-xl space-x-5 transition duration-100 ease-in-out 
+                flex justify-center items-center`}
+                onClick={() => handleLinkClick(link.to)}
+              >
+                {link.icon}
+                <span>{link.label}</span>
+              </Link>
+            ))}
             <div className="cursor-pointer text-gray-700 hover:text-primary space-x-5 transition duration-100 ease-in-out p-3 flex justify-center items-center">
               <LogoutRounded />
-              <span>Logout</span>
+              <span onClick={handleDestroyToken}>Logout</span>
             </div>
           </div>
         </div>
