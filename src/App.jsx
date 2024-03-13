bec5d20af62467e36510af2e8c168777a19e048b
Trentcrypt3d <70151180+Trentcrypt3d@users.noreply.github.com>
2024-03-13 10:15:51 +0800

    added routes to sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,86 +1,77 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-import { Routes, Route, Outlet } from 'react-router-dom'
+import { Routes, Route, Outlet } from "react-router-dom";
 
 // Hooks
-import { AuthProvider } from './hooks/AuthContext'
+import { AuthProvider } from "./hooks/AuthContext";
 
 // Pages
-import Login from './pages/Login'
-import Members from './pages/Members'
+import Login from "./pages/Login";
+import Members from "./pages/Members";
 // import Dashboard from "./pages/Dashboard";
-import SystemSettings from './pages/SystemSettings'
-import Announce from './pages/Announce'
-import Contents from './pages/Contents'
-import TestComponents from './pages/Test/PdfViewer'
-import Missing from './pages/Missing'
-import Requests from './pages/Requests'
-import Calendar from './pages/Calendar'
+import SystemSettings from "./pages/SystemSettings";
+import Announce from "./pages/Announce";
+import Contents from "./pages/Contents";
+import TestComponents from "./pages/Test/PdfViewer";
+import Missing from "./pages/Missing";
+import Requests from "./pages/Requests";
+import Calendar from "./pages/Calendar";
 
-import PrivateRoute from './components/PrivateRoute/PrivateRoute'
-import ApplicationProcessing from './pages/ApplicationProcessing/ApplicationProcessing'
-import MembershipManagement from './pages/MembershipManagement/MembershipManagement'
-import ViewActivities from './pages/MembershipManagement/ViewActivities'
-import AdminDashboard from './pages/Dashboard/Dashboard'
-import Notification from './pages/Notification/Notification'
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import ApplicationProcessing from "./pages/ApplicationProcessing/ApplicationProcessing";
+import MembershipManagement from "./pages/MembershipManagement/MembershipManagement";
+import ViewActivities from "./pages/MembershipManagement/ViewActivities";
+import AdminDashboard from "./pages/Dashboard/Dashboard";
+import Notification from "./pages/Notification/Notification";
 import ActivityMonitoring from "./pages/ActivityMonitoring/ActivityMonitoring";
-import MemberView from './pages/MembershipManagement/MemberView'
+import MemberView from "./pages/MembershipManagement/MemberView";
+import Inquiries from "./pages/Inquiries/Inquiries";
 
-import Reports from './pages/Reports/Reports'
+import Reports from "./pages/Reports/Reports";
 
 function App() {
-    const [authenticated, setAuthenticated] = useState(false) // You can set this based on the user's authentication status
+  const [authenticated, setAuthenticated] = useState(false); // You can set this based on the user's authentication status
 
-    return (
-        <>
-            <AuthProvider>
-                <Routes>
-                    {/* Public Routes */}
-                    <Route path="/" element={<Login />} />
-                    <Route path="/test" element={<TestComponents />} />
-                    <Route path='/reports' element={<Reports />}/>
-                    {/* <Route path="/dashboard" element={<Dashboard />} /> */}
-                    <Route path="/calendar" element={<Calendar />} />
-                    <Route path="/announce" element={<Announce />} />
-                    <Route
-                        path="/applications"
-                        element={<ApplicationProcessing />}
-                    />
-                    <Route
-                        path="/membership"
-                        element={<MembershipManagement />}
-                    />
-                    <Route
-                        path="/membership/:member_id"
-                        element={<MemberView />}
-                    />
-                    <Route
-                        path="/activities"
-                        element={<ViewActivities />}
-                    />
-                    <Route path="/dashboard" element={<AdminDashboard />} />
-                    <Route path="/admin/notification" element={<Notification />} />
-                    <Route
-                        path="/admin/activity_monitoring"
-                        element={<ActivityMonitoring />}
-                    />
+  return (
+    <>
+      <AuthProvider>
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<Login />} />
+          <Route path="/test" element={<TestComponents />} />
+          <Route path="/reports" element={<Reports />} />
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+          <Route path="/calendar" element={<Calendar />} />
+          <Route path="/announce" element={<Announce />} />
+          <Route path="/applications" element={<ApplicationProcessing />} />
+          <Route path="/membership" element={<MembershipManagement />} />
+          <Route path="/membership/:member_id" element={<MemberView />} />
+          <Route path="/activities" element={<ViewActivities />} />
+          <Route path="/dashboard" element={<AdminDashboard />} />
+          <Route path="/admin/notification" element={<Notification />} />
+          <Route
+            path="/admin/activity_monitoring"
+            element={<ActivityMonitoring />}
+          />
 
-                    {/* Catch */}
-                    <Route path="/missing" element={<Missing />} />
-                </Routes>
-            </AuthProvider>
-        </>
-    )
+          <Route path="/inquiries" element={<Inquiries />} />
 
-    // practice
-    // return (
-    //     <Routes>
-    //         <Route path="/admin/application_processing" element={<ApplicationProcessing />} />
-    //         <Route path="/admin/notification" element={<Notification />} />
-    //         <Route path="/dashboard" element={<AdminDashbaord />} />
-    //     </Routes>
-    // )
+          {/* Catch */}
+          <Route path="/missing" element={<Missing />} />
+        </Routes>
+      </AuthProvider>
+    </>
+  );
+
+  // practice
+  // return (
+  //     <Routes>
+  //         <Route path="/admin/application_processing" element={<ApplicationProcessing />} />
+  //         <Route path="/admin/notification" element={<Notification />} />
+  //         <Route path="/dashboard" element={<AdminDashbaord />} />
+  //     </Routes>
+  // )
 }
 
-export default App
+export default App;
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,53 +1,67 @@
-import { Fragment } from 'react';
+import { Fragment } from "react";
 import {
-    Dashboard,
-    CalendarMonth,
-    AccessTime,
-    Person,
-    HelpRounded,
-    LogoutRounded
-} from '@mui/icons-material'
+  Dashboard,
+  CalendarMonth,
+  AccessTime,
+  Person,
+  HelpRounded,
+  LogoutRounded,
+} from "@mui/icons-material";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <Fragment>
       <div className="fixed top-0 left-0 h-full bg-[#e8e8e8] w-[20rem] shadow">
-        <div className="flex items-center justify-center mt-8">
+        <div className="flex flex-col items-center justify-center mt-8">
           <img
             src="../../public/img/binno-logo.png"
             alt="binno-logo"
             className="h-[6rem] mt-10"
           />
+          <h1 className="text-xl">
+            <span>Sytem Administrator</span>
+          </h1>
         </div>
-            <div className="mt-10 justify-center items-center">
-                <div className="flex flex-col justify-start items-start space-y-5 text-xl ml-[20%]">
-                    <div className="cursor-pointer text-secondary  bg-gradient-to-tr from-white to-secondary p-2 pr-[4rem] border border-blue-500 rounded-xl space-x-5 transition duration-100 ease-in-out flex justify-center items-center">
-                        <Dashboard />
-                        <span>Dashboard</span>
-                    </div>
-                    <div className="cursor-pointer text-gray-700 hover:text-primary space-x-5 transition duration-100 ease-in-out p-3 flex justify-center items-center">
-                        <CalendarMonth />
-                        <span>Calendar</span>
-                    </div>
-                    <div className="cursor-pointer text-gray-700 hover:text-primary space-x-5 transition duration-100 ease-in-out p-3 flex justify-center items-center">
-                        <AccessTime />
-                        <span>Activities</span>
-                    </div>
-                    <div className="cursor-pointer text-gray-700 hover:text-primary space-x-5 transition duration-100 ease-in-out p-3 flex justify-center items-center">
-                        <Person />
-                        <span>Members</span>
-                    </div>
-                    <div className="cursor-pointer text-gray-700 hover:text-primary space-x-5 transition duration-100 ease-in-out p-3 flex justify-center items-center">
-                        <HelpRounded />
-                        <span>Inquiries</span>
-                    </div>
-                    <div className="cursor-pointer text-gray-700 hover:text-primary space-x-5 transition duration-100 ease-in-out p-3 flex justify-center items-center">
-                        <LogoutRounded />
-                        <span>Logout</span>
-                    </div>
-                </div>
+        <div className="mt-10 justify-center items-center">
+          <div className="flex flex-col justify-start items-start space-y-5 text-xl ml-[20%]">
+            <Link to="/dashboard">
+              <div className="cursor-pointer text-secondary  bg-gradient-to-tr from-white to-secondary p-2 pr-[4rem] border border-blue-500 rounded-xl space-x-5 transition duration-100 ease-in-out flex justify-center items-center">
+                <Dashboard />
+                <span>Dashboard</span>
+              </div>
+            </Link>
+            <Link to="/calendar">
+              <div className="cursor-pointer text-gray-700 hover:text-primary space-x-5 transition duration-100 ease-in-out p-3 flex justify-center items-center">
+                <CalendarMonth />
+                <span>Calendar</span>
+              </div>
+            </Link>
+            <Link to="/activities">
+              <div className="cursor-pointer text-gray-700 hover:text-primary space-x-5 transition duration-100 ease-in-out p-3 flex justify-center items-center">
+                <AccessTime />
+                <span>Activities</span>
+              </div>
+            </Link>
+            <Link to="/membership">
+              <div className="cursor-pointer text-gray-700 hover:text-primary space-x-5 transition duration-100 ease-in-out p-3 flex justify-center items-center">
+                <Person />
+                <span>Members</span>
+              </div>
+            </Link>
+            <Link to="/inquiries">
+              <div className="cursor-pointer text-gray-700 hover:text-primary space-x-5 transition duration-100 ease-in-out p-3 flex justify-center items-center">
+                <HelpRounded />
+                <span>Inquiries</span>
+              </div>
+            </Link>
+            <div className="cursor-pointer text-gray-700 hover:text-primary space-x-5 transition duration-100 ease-in-out p-3 flex justify-center items-center">
+              <LogoutRounded />
+              <span>Logout</span>
             </div>
+          </div>
         </div>
+      </div>
     </Fragment>
   );
 };
