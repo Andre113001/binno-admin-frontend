4c83261750261c056b4c4bdce25a3cf08f649955
Trentcrypt3d <70151180+Trentcrypt3d@users.noreply.github.com>
2024-04-04 10:35:12 +0800

    added user inquries

--- src/pages/Inquiries/User Inquiries/UserInquiries.jsx
+++ src/pages/Inquiries/User Inquiries/UserInquiries.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Sidebar from "../../../components/Sidebar/Sidebar";
+import Back from "../../../components/Back/Back";
+import UserInquiries from "./UserInquiries_data.jsx";
+import EmailRoundedIcon from "@mui/icons-material/EmailRounded";
 
 function UserInquiries() {
-  return <div>UserInquiries</div>;
+  return (
+    <>
+      <Fragment>
+        <Sidebar />
+        <div className="flex container flex-col m-10 bg-darkWhite">
+          <div className="flex justify-end">
+            <Back />
+          </div>
+          <div className="flex flex-col items-center text-5xl font-bold text-primary justify-center w-[100%] ">
+            Frequently Asked Question
+          </div>
+          {UserInquiries.map((item) => (
+            <div className="flex flex-col items-center">
+              <div
+                className="flex w-[80%] flex-col my-7 rounded bg-darkWhite"
+                key={item.id}
+              >
+                <h1>{item.email}</h1>
+                <h1 className="font-bold">{item.title}</h1>
+                <div className="flex justify-end items-center m-2">
+                  <button className="bg-secondary color-white p-2">
+                    <EmailRoundedIcon />
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </Fragment>
+    </>
+  );
 }
 
 export default UserInquiries;
---
+++ src/pages/Inquiries/User Inquiries/UserInquiries_data.jsx
@@ -0,0 +1,58 @@
+const UserInquiries = [
+  {
+    id: "1",
+    title: "How to start a company?",
+    email: "seantrent@gmail.com",
+    inquiry:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas laoreet, eros eget dapibus bibendum, lectus metus eleifend arcu, vel porta erat dolor iaculis est. Donec eu euismod massa. Pellentesque vulputate turpis augue, malesuada aliquet purus tempor nec.",
+  },
+  {
+    id: "2",
+    title: "How to start a company?",
+    email: "seantrent@gmail.com",
+    inquiry:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas laoreet, eros eget dapibus bibendum, lectus metus eleifend arcu, vel porta erat dolor iaculis est. Donec eu euismod massa. Pellentesque vulputate turpis augue, malesuada aliquet purus tempor nec.",
+  },
+  {
+    id: "3",
+    title: "How to start a company?",
+    email: "seantrent@gmail.com",
+    inquiry:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas laoreet, eros eget dapibus bibendum, lectus metus eleifend arcu, vel porta erat dolor iaculis est. Donec eu euismod massa. Pellentesque vulputate turpis augue, malesuada aliquet purus tempor nec.",
+  },
+  {
+    id: "4",
+    title: "How to start a company?",
+    email: "marriane@gmail.com",
+    inquiry:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas laoreet, eros eget dapibus bibendum, lectus metus eleifend arcu, vel porta erat dolor iaculis est. Donec eu euismod massa. Pellentesque vulputate turpis augue, malesuada aliquet purus tempor nec.",
+  },
+  {
+    id: "5",
+    title: "How to start a company?",
+    email: "Anonymous",
+    inquiry:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas laoreet, eros eget dapibus bibendum, lectus metus eleifend arcu, vel porta erat dolor iaculis est. Donec eu euismod massa. Pellentesque vulputate turpis augue, malesuada aliquet purus tempor nec.",
+  },
+  {
+    id: "6",
+    title: "How to start a company?",
+    email: "Anonymous",
+    inquiry:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas laoreet, eros eget dapibus bibendum, lectus metus eleifend arcu, vel porta erat dolor iaculis est. Donec eu euismod massa. Pellentesque vulputate turpis augue, malesuada aliquet purus tempor nec.",
+  },
+  {
+    id: "7",
+    title: "How to start a company?",
+    email: "marcus@gmail.com",
+    inquiry:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas laoreet, eros eget dapibus bibendum, lectus metus eleifend arcu, vel porta erat dolor iaculis est. Donec eu euismod massa. Pellentesque vulputate turpis augue, malesuada aliquet purus tempor nec.",
+  },
+  {
+    id: "8",
+    title: "How to start a company?",
+    email: "Anonymous",
+    inquiry:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas laoreet, eros eget dapibus bibendum, lectus metus eleifend arcu, vel porta erat dolor iaculis est. Donec eu euismod massa. Pellentesque vulputate turpis augue, malesuada aliquet purus tempor nec.",
+  },
+];
