b1ef72a6d196486642384b130f80cdf41f7198e3
Trentcrypt3d <70151180+Trentcrypt3d@users.noreply.github.com>
2024-04-09 20:57:44 +0800

    added delete modal to faq

--- src/pages/Inquiries/FAQ Lists Display/FAQ Functional List/FAQDelete.jsx
+++ src/pages/Inquiries/FAQ Lists Display/FAQ Functional List/FAQDelete.jsx
@@ -6,10 +6,105 @@ import { Fragment } from "react";
 import SearchBar from "../../../../components/Search Bar/Searchbar";
 import Sidebar from "../../../../components/Sidebar/Sidebar";
 import { Link } from "react-router-dom";
+import useCustomModal from "../../../../hooks/useCustomModal";
+import { Button } from "@mui/material";
+import Stack from "@mui/material/Stack";
 
 function FAQDelete() {
+  const [selectedId, setSelectedID] = useState();
+  const [faqId, setFaqId] = useState();
+  const {
+    handleClose: handleCloseModal,
+    handleOpen: handleOpenModal,
+    CustomModal,
+  } = useCustomModal();
+
+  const handleDeleteFAQ = async () => {
+    const res = await sendRequest({
+      url: `${import.meta.env.VITE_BACKEND_DOMAIN}/faq/delete`,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json", // Specify that you are sending JSON data
+      },
+      body: JSON.stringify({
+        faqId: selectedId,
+      }),
+    });
+
+    if (res.message === "FAQ deleted successfully") {
+      handleCloseModal();
+      showSnackbar("Blog Deleted Successfully", "success");
+      window.location.reload();
+    } else {
+      showSnackbar("Delete Unsuccessful", "error");
+    }
+  };
+
   return (
     <>
+      <CustomModal
+        handleOpen={handleOpenModal}
+        handleClose={handleCloseModal}
+        content={
+          <Fragment>
+            <div className="w-full m-0">
+              <center>
+                <h1 className="text-3xl font-bold mb-2">
+                  Are you sure you want to delete?
+                </h1>
+                <h3 className="text-lg">
+                  The content will be permanently deleted from this section
+                </h3>
+                <div className="flex flex-row">
+                  <Button
+                    sx={{
+                      marginTop: "20px",
+                      border: "1px solid #5C9FEF",
+                      width: "50%",
+                      borderRadius: "10px",
+                      padding: "10px 20px",
+                      fontSize: "18px",
+                      fontWeight: "bold",
+                      color: "#5C9FEF",
+                      "&:hover": {
+                        background: "#5C9FEF",
+                        color: "#fff",
+                      },
+                    }}
+                    onClick={handleCloseModal}
+                    // disabled={isLoading}
+                  >
+                    Cancel
+                  </Button>
+                  <Button
+                    sx={{
+                      marginTop: "20px",
+                      marginLeft: "20px",
+                      background: "#EB5858",
+                      border: "1px solid #EB5858",
+                      padding: "10px 20px",
+                      width: "50%",
+                      borderRadius: "10px",
+                      "&:hover": {
+                        background: "#E20000",
+                      },
+                      color: "#fff",
+                      fontSize: "18px",
+                      fontWeight: "bold",
+                    }}
+                    onClick={(e) => {
+                      handleDeleteFAQ();
+                    }}
+                    // disabled={isLoading}
+                  >
+                    Delete
+                  </Button>
+                </div>
+              </center>
+            </div>
+          </Fragment>
+        }
+      />
       <Fragment>
         <Sidebar />
         <div className="flex flex-col items-center container m-10 bg-darkWhite">
@@ -38,18 +133,29 @@ function FAQDelete() {
                   <h1 className="font-bold">{item.title}</h1>
                   <p>{item.inquiry}</p>
                 </div>
-                <IconButton
-                  aria-label="Edit"
-                  size="large"
-                  style={{
-                    backgroundColor: "#EB5858",
-                    color: "#fff",
-                    margin: "24px",
-                    borderRadius: "15px",
+                <Stack
+                  direction="row"
+                  alignItems="center"
+                  margin={"0 20px"}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setSelectedID(item.id);
+                    handleOpenModal();
                   }}
                 >
-                  <DeleteRoundedIcon />
-                </IconButton>
+                  <IconButton
+                    aria-label="Edit"
+                    size="large"
+                    style={{
+                      backgroundColor: "#EB5858",
+                      color: "#fff",
+                      margin: "24px",
+                      borderRadius: "15px",
+                    }}
+                  >
+                    <DeleteRoundedIcon />
+                  </IconButton>
+                </Stack>
               </div>
             ))}
           </div>
